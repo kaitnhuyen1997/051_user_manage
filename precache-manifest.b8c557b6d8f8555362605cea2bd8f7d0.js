5959191104dbbcffb49e91b92638dc56899be7a4
Kai Tran <kai.tnhuyen1997@gmail.com>
2019-09-20 18:01:05 +0700

    commit first

---
+++ precache-manifest.b8c557b6d8f8555362605cea2bd8f7d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f0978846bbd2503448583ca815832b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb339ff86ed910d1f039",
+    "url": "/static/css/main.4e48bce6.chunk.css"
+  },
+  {
+    "revision": "2c4f3fee47044bfc8b76",
+    "url": "/static/js/2.8bcb9862.chunk.js"
+  },
+  {
+    "revision": "bb339ff86ed910d1f039",
+    "url": "/static/js/main.a92da28c.chunk.js"
+  },
+  {
+    "revision": "98a115b69bd16e20d606",
+    "url": "/static/js/runtime~main.6e08d05a.js"
+  }
+]);
